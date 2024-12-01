94953f98110129b5a4b193a98ec730328f4f8e8a
Yağız Efe ALAYBAY <ygzefealyb@gmail.com>
2024-12-01 17:16:50 +0300

    final commit

--- socialBlog/blog/urls.py
+++ socialBlog/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
-    path('post/<int:id>/', views.post_details, name = 'post_details'),
+    path('post/<int:id>/', views.post_details, name = 'post_detail'),
     path('post/new/', views.post_new, name = 'post_new'),
     path('post/<int:id>/edit/', views.post_edit, name = 'post_edit'),
     path('post/<int:id>/delete/', views.post_delete, name = 'post_delete')
